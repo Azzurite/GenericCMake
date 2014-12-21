a1bf5dd20e3379026ebab2b5e0a8931a46ea1857
Azzurite <azzurite+git@gmail.com>
2014-12-22 00:16:13 +0100

    Add sample user_defined_settings.cmake to repository

---
+++ user_defined_settings.cmake
@@ -0,0 +1,13 @@
+##########################################################
+## Sample user_defined_settings.cmake                   ##
+## ----------------------------------                   ##
+## This file can be used to define additional variables ##
+## only used for specific system configurations         ##
+##########################################################
+
+###### search for boost in another directory ######
+# set (Boost_LIBRARY_DIR "P:/lib/x64/")
+
+###### Compiler flags ######
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -Wall -pedantic -Wextra")
+#set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Og -ggdb -DDEBUG")
